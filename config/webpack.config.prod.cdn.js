7474ac8955a719b2e6969452b6a569c9d4ae52cd
Callum Douglas <callum@mondago.com>
2019-03-04 16:31:40 +0000

    Update: prepping for delivery

---
+++ config/webpack.config.prod.cdn.js
@@ -0,0 +1,7 @@
+const path = require("path")
+const merge = require("webpack-merge")
+const base = require("./webpack.config.base")
+
+module.exports = merge(base, {
+    output: { path: path.join(__dirname, "../dist_cdn"), library: "ucsdk", libraryTarget: "umd", filename: "ucsdk.js" }
+})
--- config/webpack.config.prod.npm.js
+++ config/webpack.config.prod.npm.js
@@ -1,6 +1,7 @@
+const path = require("path")
 const merge = require("webpack-merge")
 const base = require("./webpack.config.base")
 
 module.exports = merge(base, {
-    output: { library: "ucsdk", libraryTarget: "umd", filename: "ucsdk.js" }
+    output: { path: path.join(__dirname, "../dist_npm"), filename: "index.js" }
 })
