1259339f243507ccf96bff8c18c11ec9d287db4c
Callum Douglas <callum@mondago.com>
2019-03-04 15:51:07 +0000

    Update: config to create a umd library added

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,4 +1,6 @@
 const merge = require("webpack-merge")
 const base = require("./webpack.config.base")
 
-module.exports = merge(base)
+module.exports = merge(base, {
+    output: { library: "ucsdk", libraryTarget: "umd", filename: "ucsdk.js" }
+})
