40a1d0759ed63e83bc4faf5c671061d5af04ec18
Callum Douglas <callum@mondago.com>
2019-03-25 09:26:45 +0000

    Update: code style

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -4,5 +4,7 @@ module.exports = {
     entry: path.join(__dirname, "../src/index.ts"),
     devtool: "sourcemap",
     resolve: { extensions: [".ts", ".js"] },
-    module: { rules: [{ test: /\.ts$/, use: "ts-loader", exclude: /node_modules/ }, { test: /\.js$/, use: "babel-loader", exclude: /node_modules/ }] }
+    module: {
+        rules: [{ test: /\.ts$/, use: "ts-loader", exclude: /node_modules/ }, { test: /\.js$/, use: "babel-loader", exclude: /node_modules/ }]
+    }
 }
