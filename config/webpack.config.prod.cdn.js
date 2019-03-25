067fa8486f021fe1e20f0baa878ac70905006bd6
Callum Douglas <callum@mondago.com>
2019-03-25 09:24:31 +0000

    Update: cdn config now works as cdn

--- config/webpack.config.prod.cdn.js
+++ config/webpack.config.prod.cdn.js
@@ -3,5 +3,5 @@ const merge = require("webpack-merge")
 const base = require("./webpack.config.base")
 
 module.exports = merge(base, {
-    output: { path: path.join(__dirname, "../dist_cdn"), library: "ucsdk", libraryTarget: "umd", filename: "ucsdk.js" }
+    output: { path: path.join(__dirname, "../dist_cdn"), library: "ucsdk", libraryTarget: "var", filename: "ucsdk.js" }
 })
