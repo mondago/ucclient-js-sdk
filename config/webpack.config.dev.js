0e6f7a1d652f640d5507ed8de102bf26b821d29a
Callum Douglas <callum@mondago.com>
2019-04-08 13:14:42 +0100

    dev config updated to build to correct folder

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -3,5 +3,5 @@ const path = require("path")
 const base = require("./webpack.config.base")
 
 module.exports = merge(base, {
-    output: { path: path.join(__dirname, "../../../TestHarnesses/ucsdkTest/lib"), filename: "index.js", library: "ucsdk", libraryTarget: "umd" }
+    output: { path: path.join(__dirname, "../dist"), filename: "index.js", library: "ucclient-sdk", libraryTarget: "umd" }
 })
