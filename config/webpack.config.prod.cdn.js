0cd2782f847cf1f7efb114a11bd9e8e4a52cd4ac
Callum Douglas <callum@mondago.com>
2020-11-30 14:17:45 +0000

    update webpack config with new merge import

--- config/webpack.config.prod.cdn.js
+++ config/webpack.config.prod.cdn.js
@@ -1,7 +1,7 @@
 const path = require("path")
-const merge = require("webpack-merge")
+const { merge } = require("webpack-merge")
 const base = require("./webpack.config.base")
 
 module.exports = merge(base, {
-    output: { path: path.join(__dirname, "../dist_cdn"), library: "ucsdk", libraryTarget: "var", filename: "ucsdk.js" }
+    output: { path: path.join(__dirname, "../dist_cdn"), library: "ucsdk", libraryTarget: "var", filename: "ucsdk.js" },
 })
